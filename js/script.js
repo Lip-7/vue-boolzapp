3281d819a140ae62246ace37809560f2ad9c7013
Lip-7 <zhighofficial@gmail.com>
2023-04-01 16:11:34 +0200

    added send message skill

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ createApp({
             contacts: contacts,
             research: '',
             idChatFlag: 1,
+            chatToView: null,
+            newMessage: '',
         }
     },
     methods: {
         getHour(fullDate) {
-            const dateDivided = fullDate.split(' ')
-            dateDivided.splice(0, 1)
-            return dateDivided[0].slice(0, dateDivided.length - 4)
+            const dateDivided = fullDate.split(' ');
+            dateDivided.splice(0, 1);
+            return dateDivided[0].slice(0, dateDivided.length - 4);
         },
         filterFriends() {
             this.contacts.forEach(user => {
@@ -26,6 +28,38 @@ createApp({
         },
         selectNewChat(id) {
             this.idChatFlag = id;
+            this.getObjectById(id);
+        },
+        getObjectById(id) {
+            const filteredList = contacts.filter(obj => obj.id == id);
+            this.chatToView = filteredList[0];
+        },
+        sendNewMessage() {
+            const newMessage = this.createNewMessage();
+            const destination = this.contacts.filter(chat => chat.id == this.idChatFlag)[0];
+            destination.messages.push(newMessage);
+            this.newMessage = ''
+        },
+        createNewMessage() {
+            return {
+                date: this.actualTime(),
+                message: this.newMessage,
+                status: 'sent'
+             }
+
+        },
+        actualTime() {
+            const today = new Date();
+            const dateTpl = { day: '2-digit', month: '2-digit', year: 'numeric' };
+            const hourTpl = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+            const actualDate = today.toLocaleString('it-IT', dateTpl);
+            const actualHour = today.toLocaleString('it-IT', hourTpl);
+            return `${actualDate} ${actualHour}`
         }
+    },
+    beforeMount() {
+        this.getObjectById(this.idChatFlag);
+    },
+    mounted() {
     },
 }).mount('#app');
