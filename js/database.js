47c5cf20c7098dd6fa11e0b86d3ec095362a6f02
Lip-7 <zhighofficial@gmail.com>
2023-04-03 13:40:53 +0200

    added tasks to do in database

--- js/database.js
+++ js/database.js
@@ -213,3 +213,19 @@ const possibleAnswers = [
     "Ma quanno te sposi?",
     "Che se fà a capodanno?",
 ];
+/* things to do:
+
+funzionalità:
+
+- sotto al nome del contatto nella parte in alto a destra, cambiare l'indicazione dello stato: visualizzare il testo "sta scrivendo..." nel timeout in cui il pc risponde, poi mantenere la scritta "online" per un paio di secondi e infine visualizzare "ultimo accesso alle xx:yy" con l'orario corretto
+- dare la possibilità all'utente di cancellare tutti i messaggi di un contatto o di cancellare l'intera chat con tutti i suoi dati: cliccando sull'icona con i tre pallini in alto a destra, si apre un dropdown menu in cui sono presenti le voci "Elimina messaggi" ed "Elimina chat"; cliccando su di essi si cancellano rispettivamente tutti i messaggi di quel contatto (quindi rimane la conversazione vuota) oppure l'intera chat comprensiva di tutti i dati del contatto oltre che tutti i suoi messaggi (quindi sparisce il contatto anche dalla lista di sinistra)
+- dare la possibilità all'utente di aggiungere una nuova conversazione, inserendo in un popup il nome e il link all'icona del nuovo contatto
+- aggiungere le emoticons, anche tramite l'utilizzo di una libreria
+
+
+graphics:
+
+- aggiungere una splash page visibile per 1s all'apertura dell'app
+- aggiungere un'icona per ingrandire o rimpicciolire il font: dovrebbe essere sufficiente aggiungere una classe al wrapper principale
+- aggiungere un'icona per cambiare la modalità light/dark: dovrebbe essere sufficiente aggiungere una classe al wrapper principale
+*/
